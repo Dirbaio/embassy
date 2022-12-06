02bb2ac45deb40196d36d0773fca2db45d8049ee
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-12-06 16:49:32 +0100

    optimize

--- examples/stm32f4/src/bin/loop_bench.rs
+++ examples/stm32f4/src/bin/loop_bench.rs
@@ -2,7 +2,12 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::future::poll_fn;
+use core::mem;
 use core::sync::atomic::{AtomicU32, Ordering};
+use core::task::Poll;
+
+use cortex_m::peripheral::{DCB, DWT};
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_futures::yield_now;
@@ -13,25 +18,41 @@ use {defmt_rtt as _, panic_probe as _};
 static COUNTER: AtomicU32 = AtomicU32::new(0);
 
 #[embassy_executor::task]
-async fn incrementTask() {
+async fn incrementTask() -> ! {
+    poll_fn(|cx| {
+        cx.waker().wake_by_ref();
+        COUNTER.fetch_add(1, Ordering::Relaxed);
+        Poll::Pending
+    })
+    .await
+    /*
     loop {
         COUNTER.fetch_add(1, Ordering::Relaxed);
         yield_now().await;
         // Timer::after(Duration::from_micros(0)).await;
     }
+     */
 }
 
 #[embassy_executor::task]
 async fn stateThread() {
+    let mut dcb: DCB = unsafe { mem::transmute(()) };
+    let mut dwt: DWT = unsafe { mem::transmute(()) };
+    dcb.enable_trace();
+    DWT::unlock();
+    dwt.enable_cycle_counter();
+    dwt.set_cycle_count(0);
+
     let mut next = Instant::from_ticks(0) + Duration::from_secs(1);
     let mut count = 0;
     Timer::at(next).await;
     loop {
         let new_count = COUNTER.load(Ordering::Relaxed);
-        let cycles = 100_000_000 / (new_count - count);
+        let cycles = DWT::cycle_count() / (new_count - count);
         info!("counter: {} ({} cycles per iteration)", new_count, cycles);
         count = new_count;
         next += Duration::from_secs(1);
+        dwt.set_cycle_count(0);
         Timer::at(next).await;
     }
 }
